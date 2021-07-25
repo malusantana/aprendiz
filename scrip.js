587529ffa47a39386ce90f0bb4523db85dea60c1
L Santana <santalu2005@yahoo.com.br>
2021-07-25 20:19:37 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", (e) =>{
+e.preventDefault();
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let data= {
+nome, email, tel, endereço,
+}
+let convertData = JSON.stringify(data);
+localStorage.setItem("lead", convertData)
+let content = document. getElementById("content")
+let carregando = "<p>carregando... </p>"
+content.innerHTML = carregando
+})
+
